be0550245441529102049f98cf31f85538a37ad7
SamRapley97 <samrapley@protonmail.com>
2022-02-21 10:09:21 +0000

    authentication set up complete

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import GlobalStyle from '../styles/GlobalStyle';
 import { darkTheme } from '../styles/themes';
@@ -12,14 +13,16 @@ const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <QueryClientProvider client={queryClient}>
+    <Auth0Provider domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string} clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string} redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}>
+      <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={darkTheme}>
-        <Normalize />
+        <Normalize /> 
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+    </Auth0Provider>
   );
 };
 
