441a54db62f6ba0ced15dce10e5238157dc2b07a
leah16c <chen.lmw@gmail.com>
2022-02-21 14:08:04 +0000

    Adding logout functionality and button to page. Authentication is working.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,26 +1,43 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import Link from 'next/link';
 import { FC } from 'react';
 import { MdOutlineNotifications, MdOutlineSearch } from 'react-icons/md';
 
 import Avatar from '../elements/Avatar';
+import Button from '../elements/Button';
 import Logo from '../elements/Logo';
 import { Container, Profile, SearchBar, SearchInput } from './styles';
 
-const Header: FC = () => (
-  <Container>
-    <Link href="/" passHref>
-      <Logo />
-    </Link>
-    <SearchBar>
-      <MdOutlineSearch />
-      <SearchInput placeholder="Search for posts, groups or people..." />
-    </SearchBar>
-    <Profile>
-      <MdOutlineNotifications />
-      <Avatar src="https://media-exp1.licdn.com/dms/image/C4E03AQEx5Dx4CgFrxg/profile-displayphoto-shrink_800_800/0/1641469515061?e=1650499200&v=beta&t=T8nY1JYzD2OgB8MJ8K6TXOkZyc0RIh3B8DunQCl8upE" />
-      Jack Waterfall
-    </Profile>
-  </Container>
-);
+const Header: FC = () => {
+  const { user, logout } = useAuth0();
+
+  const handleLogOut = () => {
+    logout({
+      returnTo: process.env.NEXT_PUBLIC_AUTH0_LOGOUT_REDIRECT_URI,
+    });
+  };
+
+  return (
+    <Container>
+      <Link href="/" passHref>
+        <Logo />
+      </Link>
+      <SearchBar>
+        <MdOutlineSearch />
+        <SearchInput placeholder="Search for posts, groups or people..." />
+      </SearchBar>
+      {user && (
+        <Profile>
+          <MdOutlineNotifications />
+          {user.picture && <Avatar src={user.picture} />}
+          {user.name}
+          <Button size="sm" onClick={handleLogOut}>
+            Log Out
+          </Button>
+        </Profile>
+      )}
+    </Container>
+  );
+};
 
 export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import { AppProps } from 'next/app';
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
-import { Auth0Provider } from '@auth0/auth0-react';
 
 import GlobalStyle from '../styles/GlobalStyle';
 import { darkTheme } from '../styles/themes';
@@ -13,15 +13,19 @@ const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Auth0Provider domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string} clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string} redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}>
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN as string}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID as string}
+      redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI}
+    >
       <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={darkTheme}>
-        <Normalize /> 
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+        <ThemeProvider theme={darkTheme}>
+          <Normalize />
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Auth0Provider>
   );
 };
