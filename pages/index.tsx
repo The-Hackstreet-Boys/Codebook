0196461aae7d68161aff4b55bda307be56f382b3
jwaterfall <jack.a.waterfall@gmail.com>
2022-02-22 23:46:23 +0000

    Added intersection observer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,34 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { FC } from 'react';
-import Button from '../components/elements/Button';
+import { FC, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import HomeLayout from '../components/HomeLayout';
 import Layout from '../components/Layout';
 import PostDisplay from '../components/PostDisplay';
 import PostForm from '../components/PostForm';
+import Button from '../components/elements/Button';
 import usePosts from '../hooks/queries/usePosts';
 
 const IndexPage: FC = () => {
-  const { data, fetchNextPage } = usePosts(1);
+  const { data, fetchNextPage } = usePosts(10);
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) fetchNextPage();
+  }, [inView, fetchNextPage]);
 
   return (
     <Layout>
       <HomeLayout>
         <PostForm />
         {data?.pages.map((page) => (
-         <>  {page.data.map((post) => (
-          <PostDisplay post={post} key={post._id} />
-        ))}</>
+          <>
+            {page.data.map((post) => (
+              <PostDisplay post={post} key={post._id} />
+            ))}
+          </>
         ))}
-      <Button onClick={() => fetchNextPage()}>Load more</Button>
+        <div ref={ref}></div>
       </HomeLayout>
     </Layout>
   );
