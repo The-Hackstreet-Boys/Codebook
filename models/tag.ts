af81b01bbb932faa8f4d2b649050405a48d2b114
leah16c <chen.lmw@gmail.com>
2022-02-23 13:57:10 +0000

    Adding tag variant.

--- models/tag.ts
+++ models/tag.ts
@@ -1,11 +1,17 @@
 import mongoose, { Document, Model, Schema, model } from 'mongoose';
 
 export interface Tag extends Document {
+  variant: 'technology' | 'tool';
   name: string;
 }
 
 const tagSchema = new Schema<Tag>(
   {
+    variant: {
+      required: true,
+      type: String,
+      enum: ['technology', 'tool'],
+    },
     name: {
       required: true,
       type: String,
