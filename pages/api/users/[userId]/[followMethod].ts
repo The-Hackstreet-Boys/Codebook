4a26710a1a06b4d0f976bdce225e4e13aa28ceb4
jwaterfall <jack.a.waterfall@gmail.com>
2022-02-24 11:04:33 +0000

    Merged follow and unfollow routes and added extra validation

--- pages/api/users/[userId]/[followMethod].ts
+++ pages/api/users/[userId]/[followMethod].ts
@@ -6,19 +6,35 @@ import connectToDatabase from '../../../../middleware/connectToDatabase';
 import UserModel from '../../../../models/user';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { userId } = req.query;
+  const { userId, followMethod } = req.query;
+  if (followMethod !== 'follow' && followMethod !== 'unfollow') {
+    res.status(404);
+    return;
+  }
 
   switch (req.method) {
     case 'POST':
       try {
         const currentUserId = req.user._id;
 
-        await UserModel.findByIdAndUpdate(userId, {
-          followers: { $push: currentUserId },
+        if (currentUserId === userId) {
+          res.status(400).send('Cannot follow yourself!');
+          return;
+        }
+
+        const method = followMethod === 'follow' ? '$push' : '$pull';
+
+        const updatedUser = await UserModel.findByIdAndUpdate(userId, {
+          [method]: { followers: currentUserId },
         });
 
+        if (!updatedUser) {
+          res.status(404).send('User not found!');
+          return;
+        }
+
         await UserModel.findByIdAndUpdate(currentUserId, {
-          following: { $push: userId },
+          [method]: { following: userId },
         });
 
         res.send('Successfully followed user!');
