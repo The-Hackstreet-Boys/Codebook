282448e69109b50e61c3ce1274bbd525a5c7be79
leah16c <chen.lmw@gmail.com>
2022-02-25 10:20:35 +0000

    Updating the comments model.

--- models/comment.ts
+++ models/comment.ts
@@ -1,18 +1,29 @@
-import mongoose, { Document, Model, Schema, model } from 'mongoose';
+import mongoose, { Document, Model, Schema, Types, model } from 'mongoose';
 
-export type CommentType = 'comment' | 'reply';
-
-export interface Comment extends Document {
-  type: CommentType;
+interface BaseComment extends Document {
   author: string;
   text: string;
   likeCount: number;
   likes: string[];
 }
 
-const commentSchema = new Schema<Comment>(
+export interface Comment extends BaseComment {
+  type: 'comment';
+  post: Types.ObjectId;
+}
+
+export interface Reply extends BaseComment {
+  type: 'reply';
+  comment: Types.ObjectId;
+}
+
+type Data = Comment | Reply;
+
+const commentSchema = new Schema<Data>(
   {
     type: { required: true, type: String, enum: ['comment', 'reply'] },
+    post: { type: Schema.Types.ObjectId, ref: 'Post' },
+    comment: { type: Schema.Types.ObjectId, ref: 'Comment' },
     author: { required: true, type: String, ref: 'User' },
     text: { required: true, type: String },
     likeCount: { type: Number, required: true, default: 0, min: 0 },
@@ -22,4 +33,4 @@ const commentSchema = new Schema<Comment>(
 );
 
 export default (mongoose.models.Comment ||
-  model<Comment>('Comment', commentSchema, 'comments')) as Model<Comment>;
+  model<Data>('Comment', commentSchema, 'comments')) as Model<Data>;
