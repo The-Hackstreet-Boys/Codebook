021c9f3057a7bbacc98b5524def7d9973860de99
leah16c <chen.lmw@gmail.com>
2022-03-02 11:41:55 +0000

    Light theme options added.

--- types/styled-components.d.ts
+++ types/styled-components.d.ts
@@ -15,6 +15,7 @@ declare module 'styled-components' {
     borderRadius: string;
     text: string;
     textSecondary: string;
+    buttonText: string;
     primary: string;
     secondary: string;
     buttonPrimaryHover: string;
