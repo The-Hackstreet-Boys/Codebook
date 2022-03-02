f8d89b8454efb7b8936af5f30997772a02ef09fc
SamRapley97 <samrapley@protonmail.com>
2022-03-02 10:56:14 +0000

    started with saved posts

--- pages/api/users/me/saved-posts.ts
+++ pages/api/users/me/saved-posts.ts
@@ -3,12 +3,19 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 import authentication from '../../../../middleware/authentication';
 import connectToDatabase from '../../../../middleware/connectToDatabase';
+import UserModel from '../../../../models/user';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case 'GET':
       try {
-        res.json(req.user);
+        const { savedPosts, _id } = req.user;
+
+        const usersSavedPosts = await UserModel.find({
+          _id: { $in: savedPosts, $ne: _id },
+        });
+
+        res.json({ savedPosts });
       } catch (err) {
         res.status(500).json({ error: (err as Error).message || err });
       }
