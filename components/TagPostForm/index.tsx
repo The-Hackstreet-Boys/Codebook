e421369ac776e8a1fb0d469bba67f9ad66150150
tommiesten <94461231+tommiesten@users.noreply.github.com>
2022-03-03 16:14:45 +0000

    started connecting the createTag function to the buttons on tagPostForm

--- components/TagPostForm/index.tsx
+++ components/TagPostForm/index.tsx
@@ -1,15 +1,22 @@
 import { FC, FormEvent, useRef } from 'react';
 import { MdTag } from 'react-icons/md';
 
+import useCreateTag from '../../hooks/mutations/useCreateTag';
+import { ExtendedPost } from '../../hooks/queries/usePosts';
 import useBoolean from '../../hooks/useBoolean';
 import useOnClickOutside from '../../hooks/useOnClickOutside';
 import { Flexbox } from '../elements/Box';
 import Button from '../elements/Button';
+import { Dropdown } from '../elements/Dropdown';
 import { TagForm, TagInput, ToggleTag } from './styles';
 
-const Tag: FC = () => {
-  const [isVisible, toggleIsVisible, setIsVisible] = useBoolean(false);
+interface Props {
+  post: ExtendedPost;
+}
 
+const Tag: FC<Props> = ({ post }) => {
+  const [isVisible, toggleIsVisible, setIsVisible] = useBoolean(false);
+  // const { mutate: createTag } = useCreateTag(post._id);
   const ref = useRef<HTMLDivElement>(null);
   useOnClickOutside(ref, () => setIsVisible(false));
 
@@ -21,12 +28,14 @@ const Tag: FC = () => {
     <ToggleTag ref={ref} isOpen={isVisible}>
       <MdTag onClick={toggleIsVisible} />
       <TagForm>
-        <Flexbox direction="row">
-          <form onSubmit={handleSubmit}>
-            <TagInput placeholder="Add a tag" />
-            <Button size="sm">+</Button>
-          </form>
-        </Flexbox>
+        <Dropdown ref={ref} isOpen={isVisible}>
+          <Flexbox direction="row">
+            <form onSubmit={handleSubmit}>
+              <TagInput placeholder="Add a tag" />
+              <Button size="sm">+</Button>
+            </form>
+          </Flexbox>
+        </Dropdown>
       </TagForm>
     </ToggleTag>
   );
