e1abbfa53a94a1f74253fe16ef346ab4788b0f00
tommiesten <94461231+tommiesten@users.noreply.github.com>
2022-03-04 11:40:29 +0000

    created useTags query

--- components/TagPostForm/index.tsx
+++ components/TagPostForm/index.tsx
@@ -1,9 +1,10 @@
-import { FC, useRef } from 'react';
+import { FC, useRef, useState } from 'react';
 import { MdTag } from 'react-icons/md';
 
 import useAssignTag from '../../hooks/mutations/useAssignTag';
 import useBoolean from '../../hooks/useBoolean';
 import useOnClickOutside from '../../hooks/useOnClickOutside';
+import { Tag } from '../../models/tag';
 import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from '../elements/Dropdown';
 
 interface Props {
@@ -14,14 +15,15 @@ const TagDropdown: FC<Props> = ({ tag }) => {
   const [isOpen, toggleIsOpen, setIsOpen] = useBoolean(false);
   const ref = useRef<HTMLDivElement>(null);
   useOnClickOutside(ref, () => setIsOpen(false));
-  const { mutate: assignTag } = useAssignTag(tag);
+  const [tags, setTags] = useState<Tag[]>([]);
+  // const { mutate: assignTag } = useAssignTag(tag);
 
   return (
     <Dropdown ref={ref} isOpen={isOpen}>
       <MdTag />
       <DropdownToggle onClick={toggleIsOpen}></DropdownToggle>
       <DropdownMenu>
-        <DropdownItem onClick={() => assignTag(tag)}>Add Tag</DropdownItem>
+        <DropdownItem onClick={() => assignTag()}>Add Tag</DropdownItem>
       </DropdownMenu>
     </Dropdown>
   );
---
+++ hooks/queries/useTags.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+import { Tag } from '../../models/tag';
+
+const getTags = async () => {
+  const { origin } = window.location;
+
+  const response = await axios.get(`${origin}/api/tags`, {
+    params: {},
+  });
+
+  const data = response.data;
+  return data;
+};
+
+const useTags = () => useQuery<Tag[]>('tags', () => getTags());
+
+export default useTags;
