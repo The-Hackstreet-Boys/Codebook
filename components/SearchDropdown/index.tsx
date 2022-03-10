4e109588e2e82fcf18d84e8e83ad96c76dc2d168
jwaterfall <jwaterfall@users.noreply.github.com>
2022-03-10 13:39:19 +0000

    Prettified Code!

--- components/SearchDropdown/index.tsx
+++ components/SearchDropdown/index.tsx
@@ -17,9 +17,10 @@ import useDebounce from '@/hooks/useDebounce';
 
 import { SearchInput } from './styles';
 
-
-interface Props {variant?: 'default'| 'chat'}
-const SearchDropdown: FC <Props> = ({variant = 'default'}) => {
+interface Props {
+  variant?: 'default' | 'chat';
+}
+const SearchDropdown: FC<Props> = ({ variant = 'default' }) => {
   const [query, setQuery] = useState('');
   const debouncedQuery = useDebounce(query);
   const { data: searchResults } = useSearchResults(debouncedQuery);
@@ -39,7 +40,7 @@ const SearchDropdown: FC <Props> = ({variant = 'default'}) => {
       </DropdownToggle>
       <DropdownMenu>
         <SearchInput
-          placeholder={variant === 'default'?"Search for tags or people":"Search for contacts"}
+          placeholder={variant === 'default' ? 'Search for tags or people' : 'Search for contacts'}
           value={query}
           onChange={handleChange}
         />
@@ -54,20 +55,27 @@ const SearchDropdown: FC <Props> = ({variant = 'default'}) => {
             {searchResults.users.map((user) => (
               <Link passHref href={`/users/${user._id}`} key={user._id}>
                 <DropdownItem>
-                  {variant === 'default'?<Profile user={user} />:<Profile user={user} href ={`/messages/users/${user._id}`}/>}
+                  {variant === 'default' ? (
+                    <Profile user={user} />
+                  ) : (
+                    <Profile user={user} href={`/messages/users/${user._id}`} />
+                  )}
                 </DropdownItem>
               </Link>
             ))}
             {variant === 'default' && hasTagResults && (
-             <> <Box margin="0.5rem 0.75rem">
-                <Typography variant="h5">Tags</Typography>
-              </Box>{searchResults.tags.map((tag) => (
-              <Link passHref href={`/tags/${tag._id}`} key={tag._id}>
-                <DropdownItem>{tag.name}</DropdownItem>
-              </Link>
-            ))}</>
+              <>
+                {' '}
+                <Box margin="0.5rem 0.75rem">
+                  <Typography variant="h5">Tags</Typography>
+                </Box>
+                {searchResults.tags.map((tag) => (
+                  <Link passHref href={`/tags/${tag._id}`} key={tag._id}>
+                    <DropdownItem>{tag.name}</DropdownItem>
+                  </Link>
+                ))}
+              </>
             )}
-            
           </>
         ) : (
           <Box margin="0.75rem">
