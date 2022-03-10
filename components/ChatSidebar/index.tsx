cca25ef4505f49049d929d4084c4a8defaebcd09
jwaterfall <jack.a.waterfall@gmail.com>
2022-03-10 13:38:38 +0000

    Created chat header compoenent and layout

--- components/ChatSidebar/index.tsx
+++ components/ChatSidebar/index.tsx
@@ -5,14 +5,12 @@ import { Flexbox } from '@/components/elements/Box';
 import useChatRooms from '@/hooks/queries/useChatRooms';
 
 import { Container } from './styles';
-import SearchDropdown from '../SearchDropdown';
 
 const ChatSidebar: FC = () => {
   const { data: chatRooms } = useChatRooms();
 
   return (
     <Container>
-      <SearchDropdown variant ='chat'/>
       <Flexbox direction="column" padding="1rem" gap="1rem">
         {chatRooms?.map((chatRoom) => {
           switch (chatRoom.type) {
